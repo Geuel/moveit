adf4fd238badb9851a0526511456b032d880ba1a
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-03-16 00:21:08 -0300

    ComplitedChallenges component

---
+++ src/components/ComplitedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/ComplitedChallenges.module.css'
+
+export function ComplitedChallenges() {
+  return (
+    <div className={styles.completedChallenges}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  )
+}
